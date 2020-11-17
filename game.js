5c2f73edce5fe8c7b3a49173fafba99defacbd35
dylanpruitt <dylan.pruitt.19@cnu.edu>
2020-11-16 21:08:00 -0500

    Started work on shop logic.

--- game.js
+++ game.js
@@ -1,6 +1,33 @@
 let flowerArrangement = [];
 let size = 3;
 
+let totalScore = 0;
+let money = 100;
+
+let inventory = [];
+let shopInventory = [];
+
+let inventoryPair = (name, amount) => {
+    return {
+        name: name,
+        amount, amount
+    };
+}
+
+let gameLoop = () => {
+    // give player starting flowers
+    // a r r a n g e m e n t
+        // input
+        // confirm finished
+        // check score
+        // if bankrupt goto lost
+    // after every three arrangements
+        // restock shop
+        // do shop
+    // if lost
+        // high score logic
+}
+
 let generateEmptyArrangement = () => {
     let EMPTY = 0;
 
@@ -47,4 +74,35 @@ let getArrangementCost = () => {
         totalCost += flowerArrangement[i].cost;
     }
     return totalCost;
+}
+
+let restockShopInventory = (numberOfItems) => {
+    for (let i = 0; i < numberOfItems; i++) {
+        let randomIndex = 0;
+        while (randomIndex == 0) {
+            randomIndex = Math.floor(Math.random() * flowers.length);
+        }
+        let flower = flowers[randomIndex](-1, -1);
+        let amount = Math.floor(Math.random() * 11) + 15;
+        addItem(shopInventory, flower.name, amount);
+    }
+}
+
+let addItem = (inventory, name, amount) => {
+    let index = returnInventoryIndex(shopInventory, name);
+    if (index != -1) {
+        inventory[index].amount += amount;
+    } else {
+        inventory.push(inventoryPair(name, amount));
+    }
+}
+
+let returnInventoryIndex = (inventory, name) => {
+    let NOT_FOUND = -1;
+    for (let i = 0; i < inventory.length; i++) {
+        if (inventory[i].name === name) {
+            return i;
+        }
+    }
+    return NOT_FOUND;
 }
